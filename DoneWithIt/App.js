69c7562a3cd0a99a4b4b58063870fd22d746471e
Anurag Bansal <anuragbansaldps@gmail.com>
2021-05-04 19:10:44 +0530

    changes-made

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -1,12 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, TouchableWithoutFeedback, View, Image, SafeAreaView, Button } from 'react-native';
-import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks';
-import WelomeScreen from './app/screens/WelomeScreen';
-import ViewImageScreen from './app/screens/ViewImageScreen';
-import AppButton from './app/components/AppButton';
+import { View, StyleSheet} from "react-native";
+
+// import { StyleSheet, Text, TouchableWithoutFeedback, View, Image, SafeAreaView, Button } from 'react-native';
+// import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks';
+// import WelomeScreen from './app/screens/WelomeScreen';
+// import ViewImageScreen from './app/screens/ViewImageScreen';
+// import AppButton from './app/components/AppButton';
+// import HelloScreen from './app/practice/screens/HelloScreen'
+
 
 export default function App() {
-    return <WelomeScreen/>
+    return (
+        <>
+            <View style={styles.container}>
+                {/* <HelloScreen /> */}
+            </View>
+        </>
+    );
+    
 
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+        // alignItems: "center",
+        // justifyContent: "center",
+        flex: 1,
+        backgroundColor: "yellow",
+        // paddingTop: 40
+        // left: 10
+    }
+})
